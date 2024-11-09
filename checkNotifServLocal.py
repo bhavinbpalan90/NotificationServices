d108cc4417373466ebe5741051374ab84375801a
bhavinbpalan90 <bhavinbpalan90@gmail.com>
2024-11-09 14:43:45 -0500

    Final Version

---
+++ checkNotifServLocal.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+# URL of the API endpoint
+#url = 'http://localhost:5087/notify/new_appointment'
+url = 'http://a04aec4ee247c4b74afe0a0b88e403bc-1942226443.us-east-1.elb.amazonaws.com/notify/new_appointment'
+
+# Headers for the request
+headers = {
+    'Content-Type': 'application/json',  # Specify the data format
+    #'Authorization': 'Bearer YOUR_ACCESS_TOKEN',  # Example for Bearer token
+    'Custom-Header': 'CustomValue'  # Additional custom headers, if needed
+}
+
+# Body of the request
+data = {
+  "recipient_email": "bhavinbpalan90@gmail.com",
+  "appointment_date": "11/10/24",
+  "appointment_time": "05:00 PM",
+  "patient_name": "Bhavin Palan",
+  "doctor_name": "Dr Patel"
+}
+
+json_data = json.dumps(data)
+
+# Sending the POST request
+response = requests.post(url, headers=headers, json=data)
+
+print(response)
+
+# Checking the response
+if response.status_code == 200:
+    print('Success:', response.json())
+else:
+    print('Failed:', response.status_code, response.text)
